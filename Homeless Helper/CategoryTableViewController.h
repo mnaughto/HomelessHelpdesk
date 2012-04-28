f61e57ac3a33128e44fab76c9f2fd2935b2955ac
Mike Naughton <macuser@Mikes-Mac.local>
2012-04-28 14:17:42 -0500

    This stuff, too.

---
+++ Homeless Helper/CategoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CategoryTableViewController.h
+//  Homeless Helper
+//
+//  Created by Mac User on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryTableViewController : UITableViewController
+
+@end
---
+++ Homeless Helper/ServiceTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ServiceTableViewController.h
+//  Homeless Helper
+//
+//  Created by Mac User on 4/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServiceTableViewController : UITableViewController
+
+@end
